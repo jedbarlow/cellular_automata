0943c3ae65c1aec384f7b7325828d094f1c35e97
Jed Barlow <jed.barlow@devfacto.com>
2013-05-27 11:31:33 -0600

    change 'let .. in ..' statements to '.. where ..'

--- ca.hs
+++ ca.hs
@@ -7,23 +7,22 @@ bitty width n = [min 1 (n .&. x) | x <- (take width (map (2^) [0..]))]
 
 rule = 110
 
-ruleLookup = let ruleBitmap  = bitty 8 rule
-                 ruleMatches = map (bitty 3) [0..7]
-             in
-                 fromList (zip ruleMatches ruleBitmap)
+ruleLookup = fromList (zip ruleMatches ruleBitmap)
+             where ruleBitmap  = bitty 8 rule
+                   ruleMatches = map (bitty 3) [0..7]
 
 evolveOne (x, y, z) = ruleLookup ! [x, y, z]
 
-evolve row = let extendedRow = [0, 0] ++ row ++ [0, 0]
-                 slidingWindowTuples = (zip3 (drop 0 extendedRow)
-                                             (drop 1 extendedRow)
-                                             (drop 2 extendedRow))
-             in map evolveOne slidingWindowTuples
+evolve row = map evolveOne slidingWindowTuples
+             where slidingWindowTuples = (zip3 (drop 0 extendedRow)
+                                               (drop 1 extendedRow)
+                                               (drop 2 extendedRow))
+                   extendedRow = [0, 0] ++ row ++ [0, 0]
 
-stringify width s = let blankOffset = (take ((width - (length s)) `div` 2) (repeat ' '))
-                        prettyState = (map (\x -> if x == 0 then ' ' else '.') s)
-                    in
-                        blankOffset ++ prettyState
+stringify width s = blankOffset ++ prettyState
+                    where blankOffset = (take ((width - (length s)) `div` 2) (repeat ' '))
+                          prettyState = (map (\x -> if x == 0 then ' ' else '.') s)
+
 
 go 0 width s = putStrLn (stringify width s)
 go n width s = putStrLn (stringify width s) >> go (n - 1) width (evolve s)
